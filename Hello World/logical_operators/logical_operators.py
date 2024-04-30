4f5d2f7014baa581d4d2dba190ca7b81885b5c1f
Vsemv <vsemeniuk999@devforth.io>
2024-04-30 16:32:22 +0300

    logical operators

---
+++ Hello World/logical_operators/logical_operators.py
@@ -0,0 +1,29 @@
+# python have three logical operators: and, or, not
+x = 1
+y = 2
+
+# print(x > 1)
+# print(y > 1)
+
+# print(x >= 1 and y > 1)
+# print(x == 1 or y < 1)
+# print(not x == 1)
+
+# print(True and True)
+# print(True or True)
+# print(not True)
+
+# print(False and False)
+# print(False or False)
+# print(not False)
+
+# print(False and True)
+# print(True or False)
+# print(not True)
+
+name = 'John'
+age = 21
+is_married = False
+
+if age > 18 and is_married == False:
+    print('Hi {}! you can find a girls of your dream here!'.format(name))
