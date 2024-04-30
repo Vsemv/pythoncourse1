77788e6fac002b053f8d6d459d865b8dfdc2434f
Vsemv <vsemeniuk999@devforth.io>
2024-04-30 15:47:24 +0300

    add sets and bollean

---
+++ Hello World/boolean/boolean.py
@@ -0,0 +1,32 @@
+# print(1 < 2)
+# print(type(True))
+
+# # comparison operators
+# print(1 == 2)
+# print(1 == 1)
+# print('hello' == 'hello')
+# print('hello' == 'Hello')
+
+# age = input('Input your age ')
+# print('Your age is ' + age)
+
+# age_old = int(input('Input your age '))
+# print('Access is permitted: ' + str(age_old >= 18))
+
+#           excercise:
+
+a = 3
+b = 6
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a <= b)
+print(a >= b)
+
+c = 't'
+d = 'T'
+print(ord(c))
+print(ord(d))
+print(c > d)
+print(c < d)
--- Hello World/sets/sets.py
+++ Hello World/sets/sets.py
@@ -5,9 +5,30 @@ empty_set = set() # создать пустое множество
 print(empty_set)
 print(type(empty_set))
 
-number_list =  [1, 43, 56]
-text_tuple = ('fge', 'erge', 'rtyuq')
+number_list =  [1, 43, 56, 3, 3, 3, 3]
+text_tuple = ('fge', 'erge', 'rtyuq', 'hi', 'hi', 'hi')
 set_from_list = set(number_list)
 set_from_tuple = set(text_tuple)
+
+set_from_list.add(777)
+set_from_tuple.add('hello')
+set_from_list.add(777) # if the element already in use in "sets", this element is not added to the sets
+set_from_tuple.add('hello') # if the element already in use in "sets", this element is not added to the sets
+
+x = set_from_list.pop()
+set_from_list.remove(3)
+set_from_list.discard(43)
+# set_from_list.remove(3)
+set_from_list.discard(3)
+set_from_list.clear()
+
 print(set_from_list)
 print(set_from_tuple)
+print(x)
+
+
+                # exercise:
+
+set_text = set('blue')
+print(set_text)
+print(type(set_text))
