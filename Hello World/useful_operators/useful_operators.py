83f50b560f5e8ce16c870599df98c1a05dc14a0a
Vsemv <vsemeniuk999@devforth.io>
2024-05-01 14:31:47 +0300

    useful operators

---
+++ Hello World/useful_operators/useful_operators.py
@@ -0,0 +1,46 @@
+# for x in range(3, 11, 2):
+#     print(x)
+# print(range(5))
+# print(list(range(5)))
+
+# letter_index = 0
+# my_string = 'sadgaerg'
+# for letter in my_string:
+#     print(letter + ' is at index ' + str(letter_index))
+#     letter_index = letter_index + 1
+
+# my_string = 'sadgaerg'
+# for index, letter in enumerate(my_string):
+#     print(str(letter) + ' is at index ' + str(index))
+
+# print('a' in 'Jack')
+# print('x' in 'Jack')
+# print(str(2) in 'Jack')
+
+# letter_list = ['a', 'b', 'c', True]
+# print('a' in letter_list)
+# print(1 in letter_list)
+# print(True in letter_list)
+
+# dict_1 = {1: 'a', 2: 'b', 3: 'c'}
+# print(1 in dict_1)
+# print(1 in dict_1.keys())
+# print(1 in dict_1.values())
+# print('c' in dict_1)
+# print('c' in dict_1.values())
+
+# print(min(1, 3, 5, 2, 7, 4, 11))
+# print(max(1, 3, 5, 2, 7, 4, 11))
+# my_list = [1, 3, 5, 2, 7, 4, 11]
+# print(min(my_list))
+# print(max('Helol'))
+# for letter in 'Helol':
+#     print(ord(letter))
+
+# from random import shuffle
+# my_list = [1, 3, 5, 2, 7, 4, 11]
+# shuffle(my_list)
+# print(my_list)
+
+# from random import randint
+# print(randint(12, 20))
