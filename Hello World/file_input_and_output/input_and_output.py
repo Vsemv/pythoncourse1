9da30efe14dd28e2c6a86e05d13915ad33b9522e
Vsemv <vsemeniuk999@devforth.io>
2024-05-15 17:52:39 +0300

    fix

--- Hello World/file_input_and_output/input_and_output.py
+++ Hello World/file_input_and_output/input_and_output.py
@@ -4,4 +4,12 @@
 # print('Output something ' + x)
 
 
-git 
+# print(1, 2, 3, sep=':', end='\n')
+# print(1, 2, 3, sep=',', end=' ')
+# print(1, 2, 3, sep=';', end='')
+
+
+lorem_ipsum_text = open('/home/vsemeniuk/Desktop/LPython/sample.txt')
+for line in lorem_ipsum_text:
+    print(line)
+lorem_ipsum_text.close()
