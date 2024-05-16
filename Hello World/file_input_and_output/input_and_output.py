051131d75c4be2cc37a7a839f2485222b192c283
Vsemv <vsemeniuk999@devforth.io>
2024-05-16 12:25:57 +0300

    read files

--- Hello World/file_input_and_output/input_and_output.py
+++ Hello World/file_input_and_output/input_and_output.py
@@ -9,7 +9,40 @@
 # print(1, 2, 3, sep=';', end='')
 
 
-lorem_ipsum_text = open('/home/vsemeniuk/Desktop/LPython/sample.txt')
-for line in lorem_ipsum_text:
-    print(line)
-lorem_ipsum_text.close()
+# lorem_ipsum_text = open('/home/vsemeniuk/Desktop/LPython/sample.txt', 'r')
+# for line in lorem_ipsum_text:
+#     print(line, end='')
+# lorem_ipsum_text.close()
+
+
+# lorem_ipsum_text = open('/home/vsemeniuk/Desktop/LPython/sample.txt', 'r')
+# for line in lorem_ipsum_text:
+#     if 'mary' in line.lower():
+#         print(line, end='')
+# lorem_ipsum_text.close()
+
+
+# with open('/home/vsemeniuk/Desktop/LPython/sample.txt', 'r') as lorem_ipsum_text_1:
+#     for line in lorem_ipsum_text_1:
+#         if 'mary' in line.lower():
+#             print(line, end='')
+
+
+# with open('/home/vsemeniuk/Desktop/LPython/sample.txt', 'r') as lorem_ipsum_text_2:
+#     line = lorem_ipsum_text_2.readline()
+#     while line:
+#         print(line, end='')
+#         line = lorem_ipsum_text_2.readline()
+
+
+# with open('/home/vsemeniuk/Desktop/LPython/sample.txt', 'r') as lorem_ipsum_text_3:
+#     lines = lorem_ipsum_text_3.readlines()
+# print(lines)
+
+# for line in lines[::-1]:
+#     print(line)
+
+
+with open('/home/vsemeniuk/Desktop/LPython/sample.txt', 'r') as lorem_ipsum_text_3:
+    text = lorem_ipsum_text_3.read()
+print(text)
