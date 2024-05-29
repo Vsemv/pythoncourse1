5353b2aebc575b4d759bc01b2561c99491bbb512
Vsemv <vsemeniuk999@devforth.io>
2024-05-29 18:04:05 +0300

    unittest methods

--- Hello World/testing/TDD/cool_game.py
+++ Hello World/testing/TDD/cool_game.py
@@ -1,4 +1,9 @@
+from random import choice
+
+
 def greet(name, isEnemy):
+    if not isinstance(isEnemy, bool):
+        raise ValueError('isEnemy must be a boolean type')
     if isEnemy:
         return f'Hello {name}! I will kill bastard!'
     else:
@@ -10,3 +15,14 @@ def eat_burgers(number):
         return 'Oh! I overate!'
     else:
         return 'Mmm! Thas was excellent!'
+    
+    
+def can_fly(name):
+    if name == 'Batman':
+        return True
+    else:
+        return False
+    
+    
+def get_arsenal():
+    return choice(('knife', 'handgun', 'machine gun'))
--- Hello World/testing/TDD/test_cool_game.py
+++ Hello World/testing/TDD/test_cool_game.py
@@ -16,6 +16,10 @@ class CoolGmaeFunctionsTest(unittest.TestCase):
         """greet() have to return 'I will kill bastard! if isEnemy == True"""
         self.assertEqual(cool_game.greet('Ivan', True), 'Hello Ivan! I will kill bastard!')
         
+    def test_greet_enemy_boolean(self):
+        with self.assertRaises(ValueError):
+            cool_game.greet('Ivan', 'Bla bla')
+        
     def test_eat_burgers(self):
         """eat_burgers() have to return 'Mmm! Thas was excellent!' if number <= 3"""
         self.assertEqual(cool_game.eat_burgers(3), 'Mmm! Thas was excellent!')
@@ -23,6 +27,21 @@ class CoolGmaeFunctionsTest(unittest.TestCase):
     def test_eat_to_much_burgers(self):
         """eat_burgers() have to return 'Oh! I overate!' if number > 3"""
         self.assertEqual(cool_game.eat_burgers(4), 'Oh! I overate!')
+        
+    def test_can_fly_batman(self):
+        """Test comment: 'Batman have to be able to fly'"""
+        self.assertTrue(cool_game.can_fly('Batman'), 'Batman have to be able to fly')
+        
+    def test_can_fly_anyone_else(self):
+        self.assertEqual(cool_game.can_fly('Bob'), False)
+        self.assertEqual(cool_game.can_fly('Jim'), False)
+        self.assertEqual(cool_game.can_fly('Kevin'), False)
+        # self.assertFalse(cool_game.can_fly('Bob'), 'Anyone else have to not be able to fly')
+        # self.assertFalse(cool_game.can_fly('Jim'), 'Anyone else have to not be able to fly')
+        # self.assertFalse(cool_game.can_fly('Kevin'), 'Anyone else have to not be able to fly')
+        
+    def test_get_arsenal(self):
+        self.assertIn(cool_game.get_arsenal(), ('knife', 'handgun', 'machine gun'))
 
 
 if __name__ == '__main__':
